51c34003f779ea7f4d44b93f6a7fcbbd5502cec1
Michael Daniel <mjdaniel7@gmail.com>
2026-07-27 20:55:38 -0400

    Create reports.sql

---
+++ reports.sql
@@ -0,0 +1,15 @@
+-- Report 1: Number of moons per planet
+
+-- Report 2: Largest planet
+
+-- Report 3: Smallest planet
+
+-- Report 4: Average planet radius
+
+-- Report 5: Planet with the strongest gravity
+
+-- Report 6: Largest moon
+
+-- Report 7: Total number of moons
+
+-- Report 8: Planets sorted by 
