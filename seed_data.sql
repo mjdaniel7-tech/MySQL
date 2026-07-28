77555ca24f8937f8389d9b22372a2aa6fd92bf1d
Michael Daniel <mjdaniel7@gmail.com>
2026-07-27 20:48:11 -0400

    Create seed_data.sql

---
+++ seed_data.sql
@@ -0,0 +1,69 @@
+-- Solar System Seed Data
+
+-- Celestial Bodies
+
+INSERT INTO celestial_bodies (...);
+
+-- Sun
+
+...
+
+-- Mercury
+
+...
+
+-- Venus
+
+...
+
+-- Earth
+
+...
+
+-- Mars
+
+...
+
+-- Jupiter
+
+...
+
+-- Saturn
+
+...
+
+-- Uranus
+
+...
+
+-- Neptune
+
+...
+
+-- Pluto (optional)
+
+...
+
+-- Moons
+
+INSERT INTO moons (...);
+
+-- Earth's Moon
+
+...
+
+-- Jupiter's moons
+
+...
+
+-- Saturn's moons
+
+...
+
+-- Uranus's moons
+
+...
+
+-- Neptune's moons
+
+..
